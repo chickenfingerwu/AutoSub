54f79b0770f7ed2d2a8e0140f3287e960cf8d0f1
Pham Thai Son <swlpham@gmail.com>
2021-03-13 18:44:43 +0700

    fixed algo.

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import librosa.display
 import IPython.display as ipd
 import pysrt
 import argparse
+from numpy.core.fromnumeric import argmax, argmin
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--audio_name', type=str, default='test.wav', help='name of audio file')
@@ -22,8 +23,8 @@ print('Starting...')
 ipd.Audio(audio_name)
 
 # load audio files with librosa
-aud, sr = librosa.load(audio_name)
-rms_aud = librosa.feature.rms(aud, frame_length=FRAME_SIZE, hop_length=HOP_LENGTH)[0]
+signal, sr = librosa.load(audio_name)
+rms_aud = librosa.feature.rms(signal, frame_length=FRAME_SIZE, hop_length=HOP_LENGTH)[0]
 
 frames = range(len(rms_aud))
 rms_test_1 = rms_aud * 10
@@ -32,7 +33,7 @@ t = librosa.frames_to_time(frames, hop_length=HOP_LENGTH)
 plt.figure(figsize=(15, 17))
 
 ax = plt.subplot(3, 1, 1)
-# librosa.display.waveplot(aud, alpha=0.5)
+librosa.display.waveplot(signal, alpha=0.5)
 plt.plot(t, rms_test_1, color="r")
 # plt.ylim((-1, 1))
 plt.title("Graph sound detection")
@@ -52,20 +53,20 @@ with open("vowels.txt", "r", encoding='utf-8') as f:
             vowels.append(word)
 
 print('Making subtitle...')
-prev_max_rms = 0
-curr_max_rms = 0
 for i in range(0, len(t), step):
     if i + step > len(t):
         step = len(t) - i
-    curr_max_rms = max(rms_test_1[i:i + step])
-    if curr_max_rms > 0.25:
+    minIndex = i + argmin(rms_test_1[i:i + step + 1])
+    maxIndex = i + argmax(rms_test_1[i:i + step + 1])
+    curr_max_rms = rms_test_1[maxIndex]
+    curr_min_rms = rms_test_1[minIndex]
+    prev_max_rms = curr_max_rms
+    if curr_max_rms > 0.2 and curr_min_rms > 0.1:
         if len(frames_have_sound) > 0:
-            if curr_max_rms - prev_max_rms > 0.3:
-                prev_max_rms = curr_max_rms
+            if curr_max_rms - prev_max_rms > 0.2:
                 continue
         soundFlag = True
         frames_have_sound.append(t[i:i + step + 1])
-        key = '%d-%d' % (i, i + step + 1)
         rms_detect.append([i, i + step + 1])
     else:
         if soundFlag:
@@ -89,21 +90,40 @@ for i in range(0, len(t), step):
                     word = vowels[counter - 1]
                 sub = pysrt.SubRipItem(counter, start=startTime, end=endTime, text=word)
 
-                counter += 1
-
                 fileSub.append(sub)
                 fileSub.save('sub.srt')
 
                 # draw detected regions
                 startIndex = rms_detect[0]
                 endIndex = rms_detect[-1]
+
+                counter += 1
+
                 ax.fill_between(t[startIndex[0]:endIndex[-1]], rms_test_1[startIndex[0]:endIndex[-1]], facecolor='b')
+
+                # mfccs = librosa.feature.mfcc(y=signal[startIndex[0]:endIndex[-1]], n_mfcc=13, sr=sr, n_fft=endIndex[-1]-startIndex[0])
+                # print(mfccs)
+
+                # startIndexSignal = 0 + (startIndex[0]) * (FRAME_SIZE - HOP_LENGTH)
+                # endIndexSignal = 0 + (endIndex[-1] + 1) * (FRAME_SIZE - HOP_LENGTH)
+                # if endIndexSignal > len(signal) - 1:
+                #     endIndexSignal = len(signal) - 1
+
+                # for i in range(startIndexSignal, endIndexSignal, 500):
+                #     end = i + 100
+                #     if i + 100 > endIndexSignal - 1:
+                #         end = endIndexSignal - 1
+
+                # ft = fft.fft(signal[i:endIndexSignal//2])
+                # magnitude = np.abs(ft)
+                # frequency = linspace(0, sr, len(magnitude))
+                # index = argmax(magnitude[:sr // 2])
+                # f1 = frequency[index]
+                # print(word, f1)
+                # plt.show()
 
             rms_detect = []
             frames_have_sound = []
 
-        prev_max_rms = curr_max_rms
-
 print('Finish!! Check the file `sub.srt`')
 plt.show()
-
---
+++ utils.py
@@ -0,0 +1,9 @@
+
+def slope(x1, y1, x2, y2):
+    m = 0
+    b = (x2 - x1)
+    d = (y2 - y1)
+    if b != 0:
+        m = (d)/(b)
+
+    return m
